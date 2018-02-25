052fe0eafca934de3c4af1bcfcf9037f743e2ab3
ozlop <oswaldo.t.lopez@gmail.com>
2018-02-24 17:56:39 -0800

    Add user loader function

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from app import login
 from datetime import datetime
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -27,3 +28,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
