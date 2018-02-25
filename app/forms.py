511759d3b93d67877924743223de4c2ca1314de2
ozlop <oswaldo.t.lopez@gmail.com>
2018-02-25 13:18:07 -0800

    Fix typos

--- app/forms.py
+++ app/forms.py
@@ -20,7 +20,7 @@ class RegistrationForm(FlaskForm):
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            raise ValidationError('Please user a different username.')
+            raise ValidationError('Please use a different username.')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
