851307fced5355b24398446dca381a07459f76d1
ozlop <oswaldo.t.lopez@gmail.com>
2018-05-18 13:41:29 -0700

    Fix syntax error

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ from hashlib import md5
 
 
 followers = db.Table('followers',
-                     db.Column('follower_id', db.Integer, db.ForeignKey(user.id')),
+                     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
                      db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
                      )
 
