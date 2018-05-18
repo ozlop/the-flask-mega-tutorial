948487ff25aa40fdb33e97dfdf475e05e2a15ca5
ozlop <oswaldo.t.lopez@gmail.com>
2018-05-18 13:48:23 -0700

    Fix typo

--- app/models.py
+++ app/models.py
@@ -23,7 +23,7 @@ class User(UserMixin, db.Model):
     followed = db.relationship(
         'User', secondary=followers,
         primaryjoin=(followers.c.follower_id == id),
-        secondaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
