bc3a616c8cd978a832def6a5cfed993099273022
ozlop <oswaldo.t.lopez@gmail.com>
2018-07-01 23:02:51 -0700

    Fix typos

--- app/forms.py
+++ app/forms.py
@@ -52,7 +52,7 @@ class ResetPasswordRequestForm(FlaskForm):
   submit = SubmitField('Request Password Reset')
   
 class ResetPasswordForm(FlaskForm):
-  password = PasswordField('Password', validators[DataRequired()])
+  password = PasswordField('Password', validators=[DataRequired()])
   password2 = PasswordField(
       'Repeat Password', validators=[DataRequired(), EqualTo('password')])
   submit = SubmitField('Request Password Reset')
