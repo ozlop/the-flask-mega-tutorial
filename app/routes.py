3e927f0973ac4c18cc942394fde7cded27613ed6
ozlop <oswaldo.t.lopez@gmail.com>
2018-07-05 23:10:58 -0700

    Add text translation view function

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for, request, g
+from flask import render_template, flash, redirect, url_for, request, g, jsonify
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
 from app.email import send_password_reset_email
@@ -8,6 +8,7 @@ from werkzeug.urls import url_parse
 from datetime import datetime
 from flask_babel import _, get_locale
 from guess_language import guess_language
+from app.translate import translate
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -181,3 +182,9 @@ def reset_password(token):
     return redirect(url_for('login'))
   return render_template('reset_password.html', form=form)
 
+@app.route('/translate', methods=['POST'])
+@login_required
+def translate_text():
+  return jsonify({'text': translate(request.form['text'],
+                                    request.form['source_language'],
+                                    request.formt['dest_language'])})
